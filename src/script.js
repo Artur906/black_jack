6b508b11b10e2a408e74a54c2d0e5cd48a4cdb9c
Artur <arturdantasrodrigues@gmail.com>
2022-02-19 12:34:01 -0300

    Interface and some interactions
    So, i just started doing the real stuff, we have a simple interface, and also have a button that makes some code run

--- src/script.js
+++ src/script.js
@@ -1,4 +1,27 @@
-let firstCard = 4
-let secondCard = 6
 
+let firstCard = 4
+let secondCard = 12
 let sum = firstCard + secondCard 
+let message = ""
+
+let hasBlackJack = false
+let itsAlive = true
+let messageEl = document.getElementById("message-el")
+function startGame() {
+
+   if (sum <= 20){
+      message = "Do you want do draw a new card?"
+   } 
+   else if (sum === 21){
+      message = "Hohoooo, you've got BlackJack"
+      hasBlackJack = true
+   }
+   else {
+      message = "Sorry, you losed!"
+      itsAlive = false
+   }
+   
+   messageEl.textContent = message
+   
+}
+
