dd6c3c2a2877e998c8fe043bea47768ac2bd0543
Artur <arturdantasrodrigues@gmail.com>
2022-02-20 18:17:24 -0300

    growing up

--- src/script.js
+++ src/script.js
@@ -1,19 +1,27 @@
 
-let firstCard = 4
-let secondCard = 12
-let sum = firstCard + secondCard 
+let cards = [9, 10]
+let sum = cards[0] + cards[1] 
 let message = ""
 
 let hasBlackJack = false
 let itsAlive = true
 let messageEl = document.getElementById("message-el")
-function startGame() {
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("card-el")
+
+// another way of get some element its using querySelector, this makes the things more flexible, once that you can grab more than elements by id   
+// let sumEl = document.querySelector("#sum-el")
+
+function startGame(){
+   renderGame()
+}
+function renderGame() {
 
    if (sum <= 20){
       message = "Do you want do draw a new card?"
    } 
    else if (sum === 21){
-      message = "Hohoooo, you've got BlackJack"
+      message = "You've got BlackJack"
       hasBlackJack = true
    }
    else {
@@ -22,6 +30,14 @@ function startGame() {
    }
    
    messageEl.textContent = message
+   cardsEl.textContent = "Cards: " + cards
+   sumEl.textContent = "Sum: " + sum
    
 }
 
+function newCard() {
+   let newCard = 5
+   cards.push(newCard)
+   sum += newCard
+   renderGame()
+}
